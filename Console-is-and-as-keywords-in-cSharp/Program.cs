55cb742800e93b2f60057b966cf1e98516d3250f
indianajim <jimrunkel@gmail.com>
2018-08-09 13:00:29 -0400

    tested if base class is derived class

---
+++ Console-is-and-as-keywords-in-cSharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_is_and_as_keywords_in_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee
+            {
+                ID = 101,
+                Name = "Mark"
+            };
+
+            //returns true, as emp is Employee
+            if (emp is Employee)
+                Console.WriteLine(emp.Name + " is Employee");
+            else
+                Console.WriteLine(emp.Name + " is not Employee");
+
+
+            //returns false, as base type cannot be converted to derived type...
+            if (emp is PermanentEmployee)
+                Console.WriteLine(emp.Name + " is PermanentEmployee");
+            else
+                Console.WriteLine(emp.Name + " is not PermanentEmployee");
+        }
+    }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PermanentEmployee : Employee
+    {
+        public int AnnualSalary { get; set; }
+    }
+
+    public class ContractEmployee : Employee
+    {
+        public int HourlySalary { get; set; }
+    }
+}

4f40e65255c17760118b0754b01579b0714d8642
indianajim <jimrunkel@gmail.com>
2018-08-08 20:08:27 -0400

    initial commits - practice code

---
+++ ConsoleFizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if number div by 3 fizz
+            //if number div by 5 buzz
+            //if both then fizzbuzz
+
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                if ((i % 3 == 0) & (i % 5 == 0))
+                {
+
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }//end for
+        }
+    }
+}

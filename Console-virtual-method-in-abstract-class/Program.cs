2ab3163c9b00f453cc17b3e42ae414803c82edce
indianajim <jimrunkel@gmail.com>
2018-08-09 13:30:14 -0400

    used virtual method to allow overrides

---
+++ Console-virtual-method-in-abstract-class/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_virtual_method_in_abstract_class
+{
+
+    public abstract class AbstractClass
+    {
+        public virtual void AbstractClassMethod()
+        {
+            Console.WriteLine("default implementation");
+        }
+    }
+
+    public class SomeClass : AbstractClass
+    {
+
+    }
+
+    public class SomeOtherClass : AbstractClass
+    {
+        public override void AbstractClassMethod()
+        {
+            Console.WriteLine("new implementation");
+        }
+    }
+    class Program
+    {
+        //q: an abstract class has a default implementation for a method. 
+        //the method's default implementation is needed in some classes but not some other classes.
+        //how can you acheive this? 
+        // answer: use "virtual" to allow override.
+
+        static void Main(string[] args)
+        {
+
+            SomeClass sc = new SomeClass();
+            sc.AbstractClassMethod();
+
+            SomeOtherClass soc = new SomeOtherClass();
+            soc.AbstractClassMethod();
+        }
+    }
+}

e978ce686e729d07cb363e1397ed951bc36b3ff1
indianajim <jimrunkel@gmail.com>
2018-08-08 21:22:38 -0400

    added diff types to an array using obj

---
+++ array-Diff-types/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_Diff_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] myArray = new object[3];
+            myArray[0] = "Bob";
+            myArray[1] = 45;
+            myArray[2] = 99;
+
+            foreach (object obj in myArray)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}

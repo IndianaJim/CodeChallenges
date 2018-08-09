39eec0a186ad1221335da3715a67edebe638c4a2
indianajim <jimrunkel@gmail.com>
2018-08-09 10:35:40 -0400

    recursive find folders-files function practice

---
+++ Console-recursive-folders-function/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_recursive_folders_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a folder path: ");
+            string path = Console.ReadLine();
+            FindFiles(path);
+        }
+
+        public static void FindFiles(string path)
+        {
+            foreach (string fileName in Directory.GetFiles(path))
+            {
+                Console.WriteLine(fileName);
+            }
+
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                FindFiles(directory);
+            }
+        }
+    }
+}

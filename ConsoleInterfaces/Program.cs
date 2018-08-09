ba5671fb17e96cbf69f5d34a03fe4a23387d71e1
indianajim <jimrunkel@gmail.com>
2018-08-09 08:45:37 -0400

    defining interfaces for now

---
+++ ConsoleInterfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInterfaces
+{
+
+
+    // interfaces allow us to develop loosely coupled systems.
+    // interfaces are very useful for dependency injection
+    //interfaces make unit testing and mocking easier
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

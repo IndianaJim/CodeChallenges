efc17c96898ede2258e95c60534d440b07379c61
indianajim <jimrunkel@gmail.com>
2018-08-08 20:38:19 -0400

    reversal of words

--- ConsoleReversals/Program.cs
+++ ConsoleReversals/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
+
+//reverse each word in a sentence string
 namespace ConsoleReversals
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string initialStr = "one two three four five";
+            string result = ReverseMyString(initialStr);
+            Console.WriteLine(result);
+        }
+
+        public static string ReverseMyString(string str)
+        {
+            string resultStr = string.Join(" ", str
+                .Split(' ')
+                .Select(x => new String(x.Reverse().ToArray())));
+
+
+            return resultStr;
         }
     }
 }

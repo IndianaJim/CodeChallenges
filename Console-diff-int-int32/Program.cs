69155f7afa38bb106e68a7a48f0faafe1f95691b
indianajim <jimrunkel@gmail.com>
2018-08-09 13:17:32 -0400

    int and Int32 are same, also string and System.String

---
+++ Console-diff-int-int32/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_diff_int_int32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int and Int32 are identical
+            int i = 10;
+            Int32 j = 10;
+
+            //also no diff between string and System.String
+        }
+    }
+}

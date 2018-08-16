67d654f2e0e691a7804008bfe5f0d4dfbf0adc8a
indianajim <jimrunkel@gmail.com>
2018-08-16 12:57:26 -0400

    check if user input is a string or a number, and CodeSignal - isLucky convert int to array of digits, sum

---
+++ CodeSignal-isLucky/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignal_isLucky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //given int "n", check if sum of first half
+            //of digits == sum of second half of digits.
+
+            int n = 1230;
+            int first = 0;
+            int second = 0;
+            string numString = n.ToString();
+            int nLength = numString.Length;
+            string[] numArray = new string[nLength];
+            int counter = 0;
+
+            for (int i = 0; i < nLength; i++)
+            {
+                numArray[i] = numString.Substring(counter, 1);
+                counter++;
+            }
+
+
+            for (int i = 0; i < (nLength / 2); i++)
+            {
+                first += int.Parse(numArray[i]);
+                Console.WriteLine("first = " + first);
+            }
+            for (int j = (nLength / 2); j < nLength; j++)
+            {
+                second += int.Parse(numArray[j]);
+                Console.WriteLine("second = " + second);
+            }
+
+            Console.WriteLine("first = " + first + " and second = " + second);
+            //if (first == second)
+            //    return true;
+            //else
+            //    return false;
+        }
+    }
+}
---
+++ Console-user-input-is-int-or-string/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_user_input_is_int_or_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter your input:");
+            string strInput = Console.ReadLine();
+
+            int result = 0;
+
+            if (int.TryParse(strInput, out result))
+            {
+                Console.WriteLine("your input is a number: " + result);
+
+            }
+            else
+            {
+                Console.WriteLine("your input is not a number: " + strInput);
+            }
+
+        }
+    }
+}

2e1fb8b3ce5b23288bdea92ab395e3844bae5d28
indianajim <jimrunkel@gmail.com>
2018-09-21 21:18:29 -0400

    word count functions

--- JS-counting-word-frequency/app.js
+++ JS-counting-word-frequency/app.js
@@ -1,9 +1,58 @@
 // Counting word frequency
 var div = document.querySelector('div');
 var textInput = document.getElementById('textBoxId');
-textInput.addEventListener('change', function(e) {
-  div.innerHTML = e.target.value;
-});
+var textValue;
 
-//textInput.textContent = 'this is a test';
-console.log(textInput.textContent);
+function getText() {
+  textValue = textInput.value;
+
+  var wordArray = splitWords(textValue);
+  var wordMap = makeWordMap(wordArray);
+  var finalWordArray = sortByCount(wordMap);
+
+  //display results
+  for (let i = 0; i < finalWordArray.length; i++) {
+    if (finalWordArray[i].total > 1) {
+      div.innerHTML =
+        div.innerHTML +
+        ' ' +
+        finalWordArray[i].name +
+        ' = ' +
+        finalWordArray[i].total +
+        '<br>';
+    } else break;
+  }
+}
+
+function splitWords(text) {
+  var wordArray = text.split(/\s+/);
+  return wordArray;
+}
+
+function makeWordMap(wordArray) {
+  var wordsMap = {};
+  wordArray.forEach(function(key) {
+    if (wordsMap.hasOwnProperty(key)) {
+      wordsMap[key]++;
+    } else {
+      wordsMap[key] = 1;
+    }
+  });
+  return wordsMap;
+}
+
+function sortByCount(wordsMap) {
+  var finalWordArray = [];
+  finalWordArray = Object.keys(wordsMap).map(function(key) {
+    return {
+      name: key,
+      total: wordsMap[key]
+    };
+  });
+
+  finalWordArray.sort(function(a, b) {
+    return b.total - a.total;
+  });
+
+  return finalWordArray;
+}

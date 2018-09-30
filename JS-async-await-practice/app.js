1bb8442197acdf2306fc14794f582dfff3fca842
indianajim <jimrunkel@gmail.com>
2018-09-29 20:40:48 -0400

    async await

---
+++ JS-async-await-practice/app.js
@@ -0,0 +1,19 @@
+// JS async await practice
+
+// without async/await first...
+
+console.log('person1 shows ticket');
+console.log('person2 shows ticket');
+
+const promiseWifeBringingTickets = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('ticket');
+  }, 3000);
+});
+
+promiseWifeBringingTickets.then(t => {
+  console.log(`person3: shows ${t}`);
+});
+
+console.log('person4 shows ticket');
+console.log('person5 shows ticket');

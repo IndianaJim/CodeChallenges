a28b49e1b2e706a26282479222a9a7029b2cb972
indianajim <jimrunkel@gmail.com>
2018-09-30 20:56:56 -0400

    more async/await

--- JS-async-await-practice/app.js
+++ JS-async-await-practice/app.js
@@ -2,18 +2,76 @@
 
 // without async/await first...
 
+// console.log('person1 shows ticket');
+// console.log('person2 shows ticket');
+
+// const promiseWifeBringingTickets = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('ticket');
+//   }, 3000);
+// });
+
+// // promiseWifeBringingTickets.then(t => {
+// //   console.log(`person3: shows ${t}`);
+// // });
+
+// // console.log('person4 shows ticket');
+// // console.log('person5 shows ticket');
+
+// const getPopcorn = promiseWifeBringingTickets.then(t => {
+//   console.log('wife: i have tickets');
+//   console.log('husband: we should go in');
+//   console.log('wife: no i am hungry');
+//   return new Promise((resolve, reject) => resolve(`${t} popcorn`));
+// });
+
+// const getButter = getPopcorn.then(t => {
+//   console.log('husband: I got some popcorn');
+//   console.log('husband: we should go in');
+//   console.log('wife: no i need butter on my popcorn');
+//   return new Promise((resolve, reject) => resolve(`${t} butter`));
+// });
+
+// getButter.then(t => console.log(t));
+
+// console.log('person4 shows ticket');
+// console.log('person5 shows ticket');
+
+//now with async/await...
+
 console.log('person1 shows ticket');
 console.log('person2 shows ticket');
 
-const promiseWifeBringingTickets = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('ticket');
-  }, 3000);
-});
+const preMovie = async () => {
+  const promiseWifeBringingTickets = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('ticket'), 3000);
+  });
 
-promiseWifeBringingTickets.then(t => {
-  console.log(`person3: shows ${t}`);
-});
+  const getPopcorn = new Promise((resolve, reject) => resolve(`popcorn`));
+
+  const getButter = new Promise((resolve, reject) => resolve(`butter`));
+
+  let ticket = await promiseWifeBringingTickets;
+
+  console.log('wife: i have tickets');
+  console.log('husband: we should go in');
+  console.log('wife: no i am hungry');
+
+  let popcorn = await getPopcorn;
+  console.log('husband: I got some popcorn');
+  console.log('husband: we should go in');
+  console.log('wife: no i need butter on my popcorn');
+
+  let butter = await getButter;
+
+  console.log('husband: i got the butter');
+  console.log('husband: anything else');
+  console.log('wife: no, we are late');
+
+  return ticket;
+};
+
+preMovie().then(m => console.log(`person 3: shows ${m}`));
 
 console.log('person4 shows ticket');
 console.log('person5 shows ticket');
